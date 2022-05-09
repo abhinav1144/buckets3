87a73184b48ab86e7af0dc44b7b49780a3895f6e
abhinav1144 <76483898+abhinav1144@users.noreply.github.com>
2022-05-09 16:12:18 +0530

    Update s3_bucket.ps1

--- s3_bucket.ps1
+++ s3_bucket.ps1
@@ -3,7 +3,9 @@ $Env:AWS_SECRET_ACCESS_KEY="$($env:key)"
 
 $inital = Initialize-AWSDefaultConfiguration -AccessKey AWS_ACCESS_KEY_ID -SecretKey AWS_SECRET_ACCESS_KEY -Region us-east-1
 
-$path = Get-ChildItem -path  C:\Users\smudunuri\Downloads\sample-s3\* -Recurse -Include *.txt
+$path2 = /home/osiuser/bucket
+
+$path = Get-ChildItem -path  $path2\* -Recurse -Include *.txt
 
 $latestfile = $path | Sort-Object -Descending -Property LastWriteTime | select -First 3
 
@@ -11,5 +13,5 @@ $latestfile.Name
 
 foreach ($copy in $latestfile.Name)
 {
-    aws s3 cp C:\Users\smudunuri\Downloads\sample-s3\$copy  s3://sample97/test/ 
+    aws s3 cp /home/osiuser/bucket/$copy  s3://sample97/test/ 
 }
